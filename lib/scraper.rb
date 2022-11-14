6857714045f8feb0b103fa9b9176f395e92198d6
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-14 04:29:49 +0300

    completed code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css(".pt-none  pb-lg  mt-none  mb-none  bg-white.inlineMobileLeft-2Yo002")
+
+courses.each {|course| puts course.text.strip}
+
+first_course = doc.css(".pt-none  pb-lg  mt-none  mb-none  bg-white.inlineMobileLeft-2Yo002")[0].children
